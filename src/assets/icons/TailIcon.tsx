3c2b441f6525ceafdaafe2688a1f832b16c9804b
Andrey Shkarin <shkarinandrew@mail.ru>
2023-04-24 13:47:57 +0300

    Иконка хвоста сообщения

--- src/assets/icons/TailIcon.tsx
+++ src/assets/icons/TailIcon.tsx
@@ -8,13 +8,12 @@ interface ITailIcon {
 
 const TailIcon: FC<ITailIcon> = ({ type }) => {
   return (
-    <svg width='14' height='8' viewBox='0 0 14 8' fill='none'>
+    <svg width='11' height='17' viewBox='0 0 11 17' fill='none'>
       <path
-        d='M1.3291 4.5L3.8578 6.89664L9.8291 1M12.8291 1L6.95676 6.87234L6.8291 7'
+        d='M9.91863 15.3625C8.95893 15.0186 6.72 11.4942 6.72 8.48562L6.71967 5.9068L6.64336 1.72046L1.12164 11.924C1.92153 16.2221 11.1182 15.7923 9.91863 15.3625Z'
+        fill={type === 'owner' ? '#007AFF' : 'white'}
         stroke={type === 'owner' ? '#007AFF' : 'white'}
-        strokeWidth='0.945076'
-        strokeLinecap='round'
-        strokeLinejoin='round'
+        strokeWidth='0.859606'
       />
     </svg>
   );
