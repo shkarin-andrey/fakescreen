b9f8daccb6cd1b4d72ac0b3a794bb6cc3c1519c9
Andrey Shkarin <shkarinandrew@mail.ru>
2023-04-25 11:19:29 +0300

    border в подвале сообщений

--- src/components/Phone/PhoneFooter/PhoneFooter.tsx
+++ src/components/Phone/PhoneFooter/PhoneFooter.tsx
@@ -9,7 +9,7 @@ const PhoneFooter: FC = () => {
     <div className='w-full pl-[6px] pb-2 pr-[10px] bg-[#F6F6F6]'>
       <div className='flex items-center gap-1 py-[5px]'>
         <AttachIcon />
-        <div className='flex items-center justify-between h-7 pl-[11px] pr-[7px] border-[0.8px] border-[#d6d1d1] rounded-[14px] bg-white w-full'>
+        <div className='flex items-center justify-between h-7 pl-[11px] pr-[7px] border-[0.8px] border-[#d6d1d1] border-solid rounded-[14px] bg-white w-full'>
           <span className='text-[#AEAEB2] text-sm'>Сообщение</span>
           <StickersIcon />
         </div>
