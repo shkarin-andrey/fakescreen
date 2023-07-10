7f36889d0dce33c6d67353287d37dc1caa937fa7
r09-Andrey <r09-andrey@monowork.pro>
2023-07-10 16:55:26 +0300

    feat: Темная тема

--- src/components/SettingsPhone/SettingsPhoneNetwork/SettingsPhoneNetwork.config.tsx
+++ src/components/SettingsPhone/SettingsPhoneNetwork/SettingsPhoneNetwork.config.tsx
@@ -4,30 +4,30 @@ import SimIcon from '../../../assets/icons/SimIcon';
 export const itemsStateSim = [
   {
     value: 4,
-    label: <SimIcon type={4} />,
+    label: <SimIcon type={4} isSettings />,
   },
   {
     value: 3,
-    label: <SimIcon type={3} />,
+    label: <SimIcon type={3} isSettings />,
   },
   {
     value: 2,
-    label: <SimIcon type={2} />,
+    label: <SimIcon type={2} isSettings />,
   },
   {
     value: 1,
-    label: <SimIcon type={1} />,
+    label: <SimIcon type={1} isSettings />,
   },
   {
     value: 0,
-    label: <SimIcon type={0} />,
+    label: <SimIcon type={0} isSettings />,
   },
 ];
 
 export const itemsNetwork = [
   {
     value: 'sim',
-    label: <SimIcon type={4} />,
+    label: <SimIcon type={4} isSettings />,
   },
   {
     value: 'avia',
