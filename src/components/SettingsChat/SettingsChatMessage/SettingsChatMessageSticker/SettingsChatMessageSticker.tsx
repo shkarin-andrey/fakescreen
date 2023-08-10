b4ba4703246ef6512fe80bfccd4b0d655c8ae720
r09-Andrey <r09-andrey@monowork.pro>
2023-08-10 16:30:59 +0400

    feat: swiper

--- src/components/SettingsChat/SettingsChatMessage/SettingsChatMessageSticker/SettingsChatMessageSticker.tsx
+++ src/components/SettingsChat/SettingsChatMessage/SettingsChatMessageSticker/SettingsChatMessageSticker.tsx
@@ -1,5 +1,10 @@
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 import { FC, memo } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 import stickers from './SettingsChatMessageSticker.config';
 import { ISettingsChatMessageSticker } from './SettingsChatMessageSticker.interface';
@@ -9,20 +14,31 @@ const SettingsChatMessageSticker: FC<ISettingsChatMessageSticker> = ({
   onSelect,
 }) => {
   return (
-    <div className='flex items-center flex-wrap gap-5'>
+    <Swiper
+      spaceBetween={20}
+      slidesPerView={5}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className='!p-2 !pb-4'
+    >
       {stickers.map((sticker: string) => (
-        <LazyLoadImage
-          key={sticker}
-          src={sticker}
-          alt={`sticker`}
-          className={`img-lazy w-20 cursor-pointer outline-offset-2 outline-green-300 ${
-            select === sticker ? 'outline outline-4' : ''
-          }`}
-          effect='blur' // opacity | black-and-white
-          onClick={() => onSelect(sticker)}
-        />
+        <SwiperSlide key={sticker}>
+          <LazyLoadImage
+            src={sticker}
+            alt={`sticker`}
+            className={`img-lazy w-20 cursor-pointer outline-offset-2 outline-green-300 ${
+              select === sticker ? 'outline outline-4' : ''
+            }`}
+            effect='blur' // opacity | black-and-white
+            onClick={() => onSelect(sticker)}
+          />
+        </SwiperSlide>
       ))}
-    </div>
+    </Swiper>
   );
 };
 
