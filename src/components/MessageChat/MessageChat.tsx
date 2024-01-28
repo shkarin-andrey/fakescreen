47bee792bfd973703bfb9e26dbcfb0a4163b158b
r09-Andrey <r09-andrey@monowork.pro>
2024-01-29 01:22:07 +0400

    fix: Редактирование сообщений с emoji

--- src/components/MessageChat/MessageChat.tsx
+++ src/components/MessageChat/MessageChat.tsx
@@ -74,11 +74,14 @@ const MessageChat: FC<IMessageChat> = ({
         }
       }
 
-      if (messageWrapperRef.current.offsetHeight > 30) {
+      if (
+        messageWrapperRef.current.offsetHeight > 30 &&
+        !matchMessageEmojiAndText?.length
+      ) {
         setWidth(messageRef.current.offsetWidth + 1);
       }
     }
-  }, [message]);
+  }, [message, messageRef.current, messageWrapperRef.current]);
 
   const bgMessage = useMemo(() => {
     if (bgImage === gallary[1]?.preview) {
